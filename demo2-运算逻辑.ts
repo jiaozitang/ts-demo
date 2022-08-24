e8d1bf4546636a3a4a9bcc91bd3ee99362bdd2e0
tangjiao12 <tangjiao11@jd.com>
2022-08-24 14:22:59 +0800

    init

--- demo2-运算逻辑.ts
+++ demo2-运算逻辑.ts
@@ -1,9 +1,15 @@
 // 条件：extends ? :
-type res1 = 1 extends 2 ? true : false;
+// 如果 T 是 2 的子类型，那么类型是 true，否则类型是 false。
+type isTwo<T> = T extends 2 ? true : false;
+// false
+type res = isTwo<1>;
 
 // 推导：infer
 // 提取元组类型的第一个元素：
-type First<Tuple extends unknown[]> = Tuple extends [infer T, ...infer R] ? T : never;
+// extends 约束类型参数只能是数组类型，因为不知道数组元素的具体类型，所以用 unknown。
+// extends 判断类型参数 T 是不是 [infer F, ...infer R] 的子类型，如果是就返回 F 变量，如果不是就不返回
+type First<T extends unknown[]> = T extends [infer F, ...infer R] ? F : never;
+// 1
 type res2 = First<[1, 2, 3]>;
 
 // 联合：｜
@@ -20,4 +26,8 @@ type MapType<T> = {
     as `${Key & string}${Key & string}${Key & string}`
     ]: [T[Key], T[Key], T[Key]]
 }
+// {
+//     aaa: [1, 1, 1];
+//     bbb: [2, 2, 2];
+// }
 type res3 = MapType<{ a: 1, b: 2 }>
--- demo5.parseQueryString.ts
+++ demo5.parseQueryString.ts
@@ -1,34 +1,28 @@
-type ParseParam<Param extends string> = 
-    Param extends `${infer Key}=${infer Value}`
-        ? {
-            [K in Key]: Value 
-        } : Record<string, any>;
-
-type MergeValues<One, Other> = 
-    One extends Other 
-        ? One
-        : Other extends unknown[]
-            ? [One, ...Other]
-            : [One, Other];
-
-type MergeParams<
-    OneParam extends Record<string, any>,
-    OtherParam extends Record<string, any>
-> = {
-  readonly [Key in keyof OneParam | keyof OtherParam]: 
-    Key extends keyof OneParam
-        ? Key extends keyof OtherParam
-            ? MergeValues<OneParam[Key], OtherParam[Key]>
-            : OneParam[Key]
-        : Key extends keyof OtherParam 
-            ? OtherParam[Key] 
-            : never
+function parseQueryString1(queryStr: string): Record<string, any> {
+    if (!queryStr || !queryStr.length) {
+        return {};
+    }
+    const queryObj = {};
+    const items = queryStr.split('&');
+    items.forEach(item => {
+        const [key, value] = item.split('=');
+        if (queryObj[key]) {
+            if (Array.isArray(queryObj[key])) {
+                queryObj[key].push(value);
+            } else {
+                queryObj[key] = [queryObj[key], value]
+            }
+        } else {
+            queryObj[key] = value;
+        }
+    });
+    return queryObj;
 }
 
-type ParseQueryString<Str extends string> = 
-    Str extends `${infer Param}&${infer Rest}`
-        ? MergeParams<ParseParam<Param>, ParseQueryString<Rest>>
-        : ParseParam<Str>;
+const res1 = parseQueryString1('a=1&b=2&c=3');
+
+
+console.log(res1.a)
 
 function parseQueryString<Str extends string>(queryStr: Str): ParseQueryString<Str> {
     if (!queryStr || !queryStr.length) {
@@ -39,7 +33,7 @@ function parseQueryString<Str extends string>(queryStr: Str): ParseQueryString<S
     items.forEach(item => {
         const [key, value] = item.split('=');
         if (queryObj[key]) {
-            if(Array.isArray(queryObj[key])) {
+            if (Array.isArray(queryObj[key])) {
                 queryObj[key].push(value);
             } else {
                 queryObj[key] = [queryObj[key], value]
@@ -51,7 +45,28 @@ function parseQueryString<Str extends string>(queryStr: Str): ParseQueryString<S
     return queryObj as any;
 }
 
-
 const res = parseQueryString('a=1&b=2&c=3');
-
 console.log(res.a)
+
+type ParseParam<Param extends string> = 
+    Param extends `${infer Key}=${infer Value}`
+        ? {
+            [K in Key]: Value 
+        } : Record<string, any>;
+
+type MergeParams<
+    OneParam extends Record<string, any>,
+    OtherParam extends Record<string, any>
+> = {
+  readonly [Key in keyof OneParam | keyof OtherParam]: 
+    Key extends keyof OneParam
+        ? OneParam[Key]
+        : Key extends keyof OtherParam 
+            ? OtherParam[Key] 
+            : never
+}
+
+type ParseQueryString<Str extends string> = 
+    Str extends `${infer Param}&${infer Rest}`
+        ? MergeParams<ParseParam<Param>, ParseQueryString<Rest>>
+        : ParseParam<Str>;
